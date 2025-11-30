d37321fb55a7c7f42ce000b62d1ccadcacec52bd
Thang Nguyen Hoang <thang.nguyenhoang@nashtechglobal.com>
2025-11-30 23:39:48 +0700

    Day 1: Initial project setup with board layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Board } from './features/board/board/board';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [Board],
+  template: '<app-board />',
 })
 export class App {
   protected readonly title = signal('task-board');
